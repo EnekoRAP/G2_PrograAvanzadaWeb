ec032c75464076a9ddd317009ed18477aa112d04
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2025-01-28 09:46:11 -0600

    Creación del proyecto donde se trabajará en la 1er práctica

---
+++ G2 - Práctica Programada 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G2___Práctica_Programada_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
