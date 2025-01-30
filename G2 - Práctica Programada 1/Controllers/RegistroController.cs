2229302069b5c3a1d3d46ca39f3933993efa1b68
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2025-01-30 02:40:32 -0600

    Login, Registro y Logo con css
    Aqui realice el login y registro(aun no oficial), tambien realice un logo ahí me dirán si les agrada, añadi el css y es todo por el momento

---
+++ G2 - Práctica Programada 1/Controllers/RegistroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace G2___Práctica_Programada_1.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
