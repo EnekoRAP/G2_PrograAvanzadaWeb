f3659b8c5df95a660397e98efcf7fa6022059921
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2025-04-18 22:27:32 -0600

    Proyecto web base del caso 2

---
+++ G2_CasoEstudio2/CasoEstudio2/Controllers/CasaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasoEstudio2.Controllers
+{
+    public class CasaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
